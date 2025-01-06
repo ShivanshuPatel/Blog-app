f6b0751b2307e87ae3a6099251ebd40681866443
Shivanshu Patel <119284473+ShivanshuPatel@users.noreply.github.com>
2025-01-06 09:43:08 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-purple-800 h-screen w-screen font-bold text-2xl">
+      Shivanshu Patel
+    
+    </div>
+  );
+}
+
+export default App;
